dd11e363e3f7f92451f92c37085ed1c6dce347eb
rutvik-kumar-patel <rutvik16302@gmail.com>
2024-01-19 01:54:26 +0530

    Added ping pong

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const healthRoutes = require('./routes/healthRoutes');
 const authRoutes = require('./routes/authRoutes');
 const toDoRoutes = require('./routes/ToDoRoutes');
 require('dotenv').config();
@@ -11,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+app.use('/',healthRoutes);
 app.use('/api',authRoutes);
 app.use('/api/todo',toDoRoutes);
 mongoose.connect(process.env.DB_URL).then((result)=>{
